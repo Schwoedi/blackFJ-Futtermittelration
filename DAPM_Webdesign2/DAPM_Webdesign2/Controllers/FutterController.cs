8c390c817e16c60d68e23f8303d382df8762a1bd
Stevie1110 <82862965+Stevie1110@users.noreply.github.com>
2023-11-10 11:23:07 +0100

    Website weitergearbeitet

--- DAPM_Webdesign2/DAPM_Webdesign2/Controllers/FutterController.cs
+++ DAPM_Webdesign2/DAPM_Webdesign2/Controllers/FutterController.cs
@@ -2,7 +2,7 @@
 
 namespace DAPM_Webdesign2.Controllers
 {
-    public class FM_Tabelle : Controller
+    public class Futter : Controller
     {
         public IActionResult Futtermittel()
         {
