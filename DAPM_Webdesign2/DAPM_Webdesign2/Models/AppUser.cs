c32ec673ed35def08a18483cf26c1339c091b238
Markus Leonhardsberger <markus.leonhardsberger2002@gmail.com>
2023-11-24 12:18:06 +0100

    Login angefangen

--- DAPM_Webdesign2/DAPM_Webdesign2/Models/AppUser.cs
+++ DAPM_Webdesign2/DAPM_Webdesign2/Models/AppUser.cs
@@ -1,6 +1,22 @@
-﻿namespace DAPM_Webdesign2.Models
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace DAPM_Webdesign2.Models
 {
-    public class AppUser
+    public class AppUser : IdentityUser
     {
+        [Key]
+        public string? Password { get; set; }
+        public string? Email { get; set; }
+        public string? firstname { get; set; }
+        public string? lastname { get; set; }
+        public string? dateofbirth { get; set; }
+        public string? streetname { get; set; }
+        public int? streetnumber { get; set; }
+        public int? plz { get; set; }
+        public string? city { get; set; }
+        public int? companynumber { get; set; }
+        
+        
     }
 }
